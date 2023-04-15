4861940d74e7f7d14456420bc85729d42857c46d
woterr <shreyasv30@Gmail.com>
2023-04-15 12:08:09 +0530

    about section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,43 @@
 function About() {
-  return <>About</>;
+  return (
+    <>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Who am I?</h1>
+          <p className="about-p">
+            As a 15-year-old IT enthusiast from Nepal, I am passionate about all
+            things tech-related. Whether it's exploring the latest programming
+            languages, experimenting with new software, or building my own
+            devices from scratch, I am constantly seeking out new ways to expand
+            my knowledge and skills in this ever-evolving field.
+          </p>
+          <p className="about-p">
+            I believe that technology has the power to change the world and make
+            a positive impact on people's lives. From improving healthcare to
+            increasing access to education, technology has the potential to
+            solve some of our greatest challenges and create a better future for
+            all.
+          </p>
+          <p className="about-p">
+            While my love for cars and hardware is an important part of my
+            interests, I am deeply committed to staying up-to-date with the
+            latest developments in the tech industry. Whether it's machine
+            learning, blockchain, or virtual reality, I am fascinated by the
+            endless possibilities for innovation and creativity in this field.
+          </p>
+          <p className="about-p">
+            Through my dedication and hard work, I hope to one day make a
+            meaningful contribution to the world of technology and inspire
+            others to pursue their own passions and interests in this exciting
+            and rapidly changing field. Whether it's through creating new
+            software applications, designing cutting-edge hardware, or finding
+            innovative ways to apply technology to real-world problems, I am
+            eager to see where my journey will take me.
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default About;
